33cf285634fe5afb5925a1a5b5e1b68c4fc9210c
Casey Brant <casey@caseybrant.com>
2017-01-15 15:10:29 -0500

    commit d3 lib and template html/js

---
+++ template.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var width = 640,
+      height = 480,
+      chart = d3.select('#chart').append('svg')
+                .attr('width', width)
+                .attr('height', height);
+});
